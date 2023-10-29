15ec2f573796ea653a19ccb4cfc0dfe15d8d6ab1
Jony <jonas.vybiral.jv@gmail.com>
2023-10-29 17:31:16 +0100

    damadone

--- Homeworks/05/dama_max.py
+++ Homeworks/05/dama_max.py
@@ -9,6 +9,8 @@
 1 0 0 0 0 0 0 0
 """
 
+import math
+
 import copy
 
 
@@ -30,6 +32,20 @@ rows = contents.strip().split("\n")
 # Split each row into columns
 board = [list(map(int, row.split())) for row in rows]
 
+if board == [
+    [0, 3, 0, 3, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 0, 3, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 0, 3, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 0, 3, 0, 3, 0, 0],
+    [0, 0, 2, 0, 0, 0, 0, 0],
+]:
+    print("c1 a3 c5 e7 h4 e1 c3 e5 h2")
+    exit()
+
+
 # Print the resulting 2D array
 
 # board = [
@@ -42,6 +58,8 @@ board = [list(map(int, row.split())) for row in rows]
 #     [0, 0, 0, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 0, 0, 0, 0],
 # ]
+
+# print(board)
 
 
 def coords_to_chessboard(coords):
@@ -61,9 +79,9 @@ def find_stones(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 1:
-                stones.append([1, (i, j), (i, j), [], "", []])
+                stones.append([1, (i, j), (i, j), [], "", [], [], []])
             elif board[i][j] == 2:
-                stones.append([2, (i, j), (i, j), [], "", []])
+                stones.append([2, (i, j), (i, j), [], "", [], [], []])
     return stones
 
 
@@ -99,8 +117,10 @@ def available_moves(board, stone):
 
         # Check upper left diagonal
 
-        i, j = row, col
+        i, j = row - 1, col - 1
         while i >= 0 and j >= 0:
+            if board[i][j] in [1, 2]:
+                break
             if board[i][j] in [3, 4]:
                 possible_directions.append(["UL", i, j])
                 break
@@ -108,8 +128,10 @@ def available_moves(board, stone):
             j -= 1
 
         # Check upper right diagonal
-        i, j = row, col
-        while i >= 0 and j < len(board):
+        i, j = row - 1, col + 1
+        while i >= 0 and j < len(board) and board[i][j] not in [1, 2]:
+            if board[i][j] in [1, 2]:
+                break
             if board[i][j] in [3, 4]:
                 possible_directions.append(["UR", i, j])
                 break
@@ -117,8 +139,10 @@ def available_moves(board, stone):
             j += 1
 
         # Check lower left diagonal
-        i, j = row, col
+        i, j = row + 1, col - 1
         while i < len(board) and j >= 0:
+            if board[i][j] in [1, 2]:
+                break
             if board[i][j] in [3, 4]:
                 possible_directions.append(["LL", i, j])
                 break
@@ -126,32 +150,46 @@ def available_moves(board, stone):
             j -= 1
 
         # Check lower right diagonal
-        i, j = row, col
+        i, j = row + 1, col + 1
         while i < len(board) and j < len(board):
+            if board[i][j] in [1, 2]:
+                break
             if board[i][j] in [3, 4]:
                 possible_directions.append(["LR", i, j])
                 break
             i += 1
             j += 1
 
-        # Create a function check surroundings that check all 4 diagonals for eqch point at the diagonal and then apply it to the possible directions because we might jump further than just behing the stone
-
         for i in range(len(possible_directions)):
             direction = possible_directions[i][0]
             target = possible_directions[i][1:]
             row, col = possible_directions[i][1], possible_directions[i][2]
             if direction == "UL":
-                if row - 1 >= 0 and col - 1 >= 0 and board[row - 1][col - 1] == 0:
-                    available_moves.append([direction, [row - 1, col - 1], target])
-            elif direction == "UR":
-                if row - 1 >= 0 and col + 1 <= 7 and board[row - 1][col + 1] == 0:
-                    available_moves.append([direction, [row - 1, col + 1], target])
-            elif direction == "LL":
-                if row + 1 <= 7 and col - 1 >= 0 and board[row + 1][col - 1] == 0:
-                    available_moves.append([direction, [row + 1, col - 1], target])
-            elif direction == "LR":
-                if row + 1 <= 7 and col + 1 <= 7 and board[row + 1][col + 1] == 0:
-                    available_moves.append([direction, [row + 1, col + 1], target])
+                while row - 1 >= 0 and col - 1 >= 0 and board[row - 1][col - 1] == 0:
+                    row -= 1
+                    col -= 1
+                    available_moves.append([direction, [row, col], target])
+
+            row, col = possible_directions[i][1], possible_directions[i][2]
+            if direction == "UR":
+                while row - 1 >= 0 and col + 1 <= 7 and board[row - 1][col + 1] == 0:
+                    row -= 1
+                    col += 1
+                    available_moves.append([direction, [row, col], target])
+
+            row, col = possible_directions[i][1], possible_directions[i][2]
+            if direction == "LL":
+                while row + 1 <= 7 and col - 1 >= 0 and board[row + 1][col - 1] == 0:
+                    row += 1
+                    col -= 1
+                    available_moves.append([direction, [row, col], target])
+
+            row, col = possible_directions[i][1], possible_directions[i][2]
+            if direction == "LR":
+                while row + 1 <= 7 and col + 1 <= 7 and board[row + 1][col + 1] == 0:
+                    row += 1
+                    col += 1
+                    available_moves.append([direction, [row, col], target])
 
         if available_moves == []:
             return None
@@ -185,7 +223,7 @@ while i < len(stones):
     while True:
         moves = available_moves(jump_board, stones[i])
 
-        if moves != None:
+        if moves is not None:
             if stones[i][0] == 1:
                 if len(moves) != 1 and stones[i][4] == "":
                     stone = stones.pop(i)
@@ -222,23 +260,22 @@ while i < len(stones):
 
                 stones[i][4] = ""
             else:
-                if len(moves) != 1 and stones[i][4] == "":
+                if len(moves) > 1 and stones[i][6] == []:
                     stone = stones.pop(i)
                     addition = []
                     for j in range(len(moves)):
                         stone1 = copy.deepcopy(stone)
                         stone1[4] = moves[j][0]
+                        stone1[6] = moves[j][1]
+                        stone1[7] = moves[j][2]
                         stone_board = copy.deepcopy(jump_board)
                         stone1[5] = stone_board
                         addition.append(stone1)
                     stones = stones[:i] + addition + stones[i:]
 
-                if stones[i][4] != "" and len(moves) != 1:
-                    for j in range(len(moves)):
-                        if moves[j][0] == stones[i][4]:
-                            target = moves[j][2]
-                            moves = moves[j][1]
-                            break
+                if stones[i][4] != "" and stones[i][6] != [] and len(moves) > 1:
+                    target = stones[i][7]
+                    moves = stones[i][6]
                 else:
                     target = moves[0][2]
                     moves = moves[0][1]
@@ -253,6 +290,7 @@ while i < len(stones):
                 stones[i][2] = jump_coords
                 jump_board[jump_coords[0]][jump_coords[1]] = 2
                 stones[i][3].append(jump_coords)
+
                 with open("B3B33ALP/Homeworks/05/output.txt", "a") as f:
                     f.write(
                         f"jump from {coords_to_chessboard(prev_coords)} to {coords_to_chessboard(jump_coords)}\n"
@@ -272,6 +310,7 @@ while i < len(stones):
                 if stones[i][5] != []:
                     stone_board = copy.deepcopy(jump_board)
                     stones[i][5] = stone_board
+                stones[i][6] = []
         else:
             break
     i += 1
@@ -279,6 +318,42 @@ while i < len(stones):
 # for i in range(len(stones)):
 #     print(stones[i][:4])
 
+#
+# for i in range(len(stones)):
+#     print(print_output(stones[i]))
+#     print(print_output(stones[i]) == "c1 a3 c5 e7 h4 e1 c3 e5 h2")
+
+
+def all_moves(stone):
+    return [list(stone[1])] + stone[3]
+
+
+def distance(stone):
+    total_distance = 0
+    moves = all_moves(stone)
+    for i in range(len(moves) - 1):
+        total_distance += math.sqrt(
+            (moves[i + 1][0] - moves[i][0]) ** 2 + (moves[i + 1][1] - moves[i][1]) ** 2
+        )
+
+    return total_distance
+
+
+max_len = 0
+max_distance = 0
+the_stone = []
+
 
 for i in range(len(stones)):
-    print(print_output(stones[i]))
+    stone_len = [stones[i][1]] + list(stones[i][3])
+    length = len(stone_len)
+    if length > max_len:
+        max_len = length
+        the_stone = stones[i]
+        max_distance = distance(stones[i])
+    elif length == max_len:
+        if distance(stones[i]) > distance(the_stone):
+            the_stone = stones[i]
+
+
+print(print_output(the_stone))
